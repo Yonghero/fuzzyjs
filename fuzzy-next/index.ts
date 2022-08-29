ad3f6db79760d299ed3ae899890779847acb2b37
younghero <yxzhang@hitotek.com>
2022-08-29 15:28:34 +0800

    fix: tailwind

--- fuzzy-next/index.ts
+++ fuzzy-next/index.ts
@@ -1,3 +1,4 @@
+import './tailwind.css'
 export { createFuzzy, globalFormItems, $shallowUpdate, $forceUpdate, $insideReactiveValue } from './runtime-core'
 export { mergeFuzzyOptions } from './utils'
 export { DefaultLayoutProvider } from './impl-layout-provider'
